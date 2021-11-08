b6e37403ec83addfcd9c0fb2c8a05b5add9975e1
Gabriel <gabriel.angelsad1@gmail.com>
2021-11-08 20:30:47 -0300

    make order controller

---
+++ src/modules/admin/controllers/order.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthRequired } from 'modules/common/guards/token';
+import { enRoles } from 'modules/database/interfaces/user';
+import { Order } from 'modules/database/models/order';
+
+import { OrderRepository } from '../repositories/order';
+import { OrderService } from '../services/order';
+import { ListValidator } from '../validators/order/list';
+import { SaveValidator } from '../validators/order/save';
+
+@ApiTags('Admin: Order')
+@Controller('/order')
+@AuthRequired([enRoles.admin])
+export class OrderController {
+  constructor(private orderRepository: OrderRepository, private orderService: OrderService) {}
+
+  @Get()
+  @ApiResponse({ status: 200, type: [Order] })
+  public async list(@Query() model: ListValidator) {
+    return this.orderRepository.list(model);
+  }
+
+  @Post()
+  @ApiResponse({ status: 200, type: Order })
+  public async save(@Body() model: SaveValidator) {
+    return this.orderService.create(model);
+  }
+}
