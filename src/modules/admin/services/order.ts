a8b1abc4342028568067e0598bc3ca30bf854f39
Gabriel <gabriel.angelsad1@gmail.com>
2021-11-08 20:31:18 -0300

    make order service

---
+++ src/modules/admin/services/order.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { IOrder } from 'modules/database/interfaces/order';
+import { Order } from 'modules/database/models/order';
+
+import { OrderRepository } from '../repositories/order';
+
+@Injectable()
+export class OrderService {
+  constructor(private orderRepository: OrderRepository) {}
+
+  public async create (model: Omit<IOrder, 'id'>): Promise<Order> {
+    const order = await this.orderRepository.insert(model);
+
+    return order;
+  }
+
+}
